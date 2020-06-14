63dc5868acd451e225de136da5ebde421b2ee1df
johnsonn <johannes.sonn@gmx.de>
2020-06-14 23:47:25 +0200

    "Remap" capslock to escape
    Added a hack that *remaps* the capslock key to the escape key without needing to install any software on the host computer.

--- mouse_wiggler.ino
+++ mouse_wiggler.ino
@@ -1,37 +1,51 @@
-#include <Mouse.h>
+#include "HID-Project.h"
+
+boolean wiggleMouse = true;
 
 void setup() {
   int dela=5;
   int x = 50;
   
   Mouse.begin();
+  BootKeyboard.begin();
   delay(1000);
-  
-  // Let the mouse pointer dance a bit in the beginning
-  for(int j = x; j > 0; j-=10){
-    for(int i = 0; i < j/2; i++){
-      Mouse.move(1, -1, 0);
-      delay(dela);
-    }
-    for(int i = 0; i < j/2; i++){
-      Mouse.move(-1, -1, 0);
-      delay(dela);
-    }
-    for(int i = 0; i < j/2; i++){
-      Mouse.move(-1, 1, 0);
-      delay(dela);
-    }
-    for(int i = 0; i < j/2; i++){
-      Mouse.move(1, 1, 0);
-      delay(dela);
+
+  wiggleMouse = !(BootKeyboard.getLeds() & LED_CAPS_LOCK);
+
+  if(wiggleMouse){
+    // Let the mouse pointer dance a bit in the beginning
+    for(int j = x; j > 0; j-=10){
+      for(int i = 0; i < j/2; i++){
+        Mouse.move(1, -1, 0);
+        delay(dela);
+      }
+      for(int i = 0; i < j/2; i++){
+        Mouse.move(-1, -1, 0);
+        delay(dela);
+      }
+      for(int i = 0; i < j/2; i++){
+        Mouse.move(-1, 1, 0);
+        delay(dela);
+      }
+      for(int i = 0; i < j/2; i++){
+        Mouse.move(1, 1, 0);
+        delay(dela);
+      }
     }
   }
 }
 
 void loop() {
-  // move the mouse every 10 seconds
-  Mouse.move(3, 0, 0);
-  delay(100);
-  Mouse.move(-3, 0, 0);
-  delay(10000);
+  for(unsigned int i = 0; i < 1000; i++){
+    if (BootKeyboard.getLeds() & LED_CAPS_LOCK){
+      BootKeyboard.write(KEY_CAPS_LOCK);
+      BootKeyboard.write(KEY_ESC);
+    }
+    delay(1);
+  }
+  if(wiggleMouse){
+    Mouse.move(1, 0, 0);
+    delay(1);
+    Mouse.move(-1, 0, 0);
+  }
 }
